5ce466faf5d10135154075d9233078cea035de89
Somendrat22 <somendrat22@gmail.com>
2024-06-13 22:21:35 +0530

    encapsulation assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+//        rwOnly.name = "Somendra";
+//        System.out.println(rwOnly.name);
+        rwOnly.setName("Somendra");
+        System.out.println(rwOnly.getName());
+    }
 }
